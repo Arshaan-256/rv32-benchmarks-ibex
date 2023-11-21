d1b247120f97d3263d6fe7d7964e85534fbfcb75
Arshaan-256 <arshaan256@gmail.com>
2023-11-21 05:50:48 -0500

    Minor fix.

--- simple-programs/rw_counter_registers.d
+++ simple-programs/rw_counter_registers.d
@@ -25,7 +25,7 @@
 #         the counters will start counting.
 
 # <start>
-# The first and second instructions must be updated to store PMU base address in x1.
+# The first and second instructions must be updated to store PMU counter base address in x1.
 80000000:       lui x1,66564
 80000004:       addi x1,x1,20
 # The third and fourth instructions must be updated to store PMU DSPM address in x7.
@@ -33,30 +33,30 @@
 8000000c:       addi x7,x7,0
 # The fifth instruction must be updated to store `NUM_COUNTER`.
 80000010:       addi x2,x0,4
-# The sixth instruction must be updated to store the size of `COUNTER_BUNDLE` in bytes.
-80000014:       addi x3,x0,16
-# The seventh instruction contains the incremental value that is written to each register.
+# The sixth and seventh instruction must be updated to store the size of `COUNTER_BUNDLE` in bytes.
+80000014:       lui  x3,0
+80000018:       addi x3,x0,16
+# The eightth instruction contains the incremental value that is written to each register.
 # Set up loop variables. Skip the `PMU_BUNDLE`.
-80000018:       add x4,x0,x0
-8000001c:       add x5,x1,x0
-80000020:       addi x6,x0,271
+8000001c:       add x4,x0,x0
+80000020:       add x5,x1,x0
+80000024:       addi x6,x0,271
 # Loop starts.
-80000024:       sw x6,0(x5)
-80000028:       addi x6,x6,271
-8000002c:       sw x6,4(x5)
-80000030:       addi x6,x6,271
-80000034:       sw x6,8(x5)
-80000038:       addi x6,x6,271
-8000003c:       sw x6,12(x5)
-80000040:       addi x6,x6,271
+80000028:       sw x6,0(x5)
+8000002c:       addi x6,x6,271
+80000030:       sw x6,4(x5)
+80000034:       addi x6,x6,271
+80000038:       sw x6,8(x5)
+8000003c:       addi x6,x6,271
+80000040:       sw x6,12(x5)
+80000044:       addi x6,x6,271
 # Update loop variables.
-80000044:       addi x4,x4,1
-80000048:       add x5,x5,x3
+80000048:       addi x4,x4,1
+8000004c:       add x5,x5,x3
 # Check if `x4 == x2` (`NUM_COUNTER`). If yes, go to end of program. Otherwise, go to <start+0x1c>.
-8000004c:       bne x4,x2,-40
+80000050:       bne x4,x2,-40
 # Program ends by writing a 101 to the PMU DSPM (`x7`) and then loop forever.
-80000050:       addi x8,x0,101
-80000054:       sw x8,0(x7)
-80000058:       add x0,x0,x0
-8000005c:       beq x0,x0,-4
-
+80000054:       addi x8,x0,101
+80000058:       sw x8,0(x7)
+8000005c:       add x0,x0,x0
+80000060:       beq x0,x0,-4
